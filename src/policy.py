cf59ff047d055f0e2bffe568d75e515ae825a8df
Anusha Ihalapathirana <aihalapathirana@gmail.com>
2026-05-04 11:27:16 +0300

    fixed lint issues

--- src/policy.py
+++ src/policy.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+
 def get_positive_policy(ite):
     # Create a policy that assigns treatment if the predicted effect is positive
     policy = (ite > 0).astype(int)
-    
+
     return policy
+
 
 def get_fraction_policy(ite, fraction):
     # Create a policy that assigns treatment to the top k individuals with the highest predicted effect
@@ -19,8 +21,9 @@ def get_fraction_policy(ite, fraction):
 
     return policy
 
+
 def get_threshold_policy(ite, threshold):
     # Create a policy that assigns treatment if the predicted effect is above a certain threshold
     policy = (ite > threshold).astype(int)
-    
+
     return policy
